b8ec27c8fa8ea2ae58ef1180dcf35a61ea9da3e6
DeamChain CEO <103885931+leopawel@users.noreply.github.com>
2023-03-22 13:13:04 +0100

    fix: guide

--- test/index.test.ts
+++ test/index.test.ts
@@ -1,8 +1,14 @@
 import test from 'tape';
 
+import {getAddressFromEns} from '../src'
 // Test results specified in original ENS Proposal:
 // https://github.com/ethereum/EIPs/issues/137
 
 test('empty name', (t) => {
 	t.plan(1)
+	const domain = "robin.arb"
+    getAddressFromEns(domain).then(address=>{
+		console.log(domain, address)
+	})
+    
 })
